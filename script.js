c3ba8a71ad58af609f7c6b816f2d87360c6a4a5b
svendoejaaren <svendoejaaren@hotmail.com>
2023-04-16 15:56:00 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,96 @@
+// FORM
+const section1 = document.querySelector('section:first-of-type')
+const p1 = document.querySelector('p:first-of-type')
+const form = document.querySelector('form')
+const section2 = document.querySelector('section:nth-of-type(2)')
+const p2 = document.querySelector('p:nth-of-type(2)')
+const p3 = document.querySelector('p:nth-of-type(3)')
+const p4 = document.querySelector('p:last-of-type')
+const section3 = document.querySelector('section:last-of-type')
+const vorigeButton = document.querySelector('a:first-of-type')
+const volgendeButton = document.querySelector('a:last-of-type')
+
+let currentSection = 0
+section1.classList.add('showForm')
+
+if (currentSection === 0) {
+    vorigeButton.classList.add('backButton')
+    p1.classList.add('page')
+}
+
+volgendeButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    currentSection++
+
+    if (currentSection > 0 && currentSection < 2) {
+        vorigeButton.classList.remove('backButton')
+        section1.classList.remove('showForm')
+        section2.classList.add('showForm')
+        p1.classList.remove('page')
+        p2.classList.add('page')
+    }
+
+    if (currentSection > 1 && currentSection < 3) {
+        section2.classList.remove('showForm')
+        section3.classList.add('showForm')
+        p2.classList.remove('page')
+        p3.classList.add('page')
+        volgendeButton.innerHTML = 'Registreer'
+    }
+})
+
+// API
+const fetchLevel1FightingPokemon = async () => {
+    await fetch('https://pokeapi.co/api/v2/type/2')
+        .then((res) => res.json())
+        .then((data) => {
+            // Create object to get specific info from the list
+            const pokemon = {
+                name: data.pokemon[21].pokemon.name,
+                url: data.pokemon[21].pokemon.url,
+            }
+
+            // Fetch the url from that pokemon
+            fetch(pokemon.url)
+                .then((res) => res.json())
+                .then((data) => {
+                    // Create object to select the image from the single pokemon
+                    const singlePokemon = {
+                        image: data.sprites['front_default'],
+                    }
+                    document.querySelector(
+                        'figure:first-of-type'
+                    ).innerHTML += `
+            <img src="${singlePokemon.image}" alt="Fighting Pokemon"/>
+            `
+                })
+        })
+}
+
+const fetchLevel2FightingPokemon = async () => {
+    await fetch('https://pokeapi.co/api/v2/type/2')
+        .then((res) => res.json())
+        .then((data) => {
+            // Create object to get specific info from the list
+            const pokemon = {
+                name: data.pokemon[4].pokemon.name,
+                url: data.pokemon[4].pokemon.url,
+            }
+
+            // Fetch the url from that pokemon
+            fetch(pokemon.url)
+                .then((res) => res.json())
+                .then((data) => {
+                    // Create object to select the image from the single pokemon
+                    const singlePokemon = {
+                        image: data.sprites['front_default'],
+                    }
+                    document.querySelector('figure:last-of-type').innerHTML += `
+            <img src="${singlePokemon.image}" alt="Fighting Pokemon"/>
+            `
+                })
+        })
+}
+
+fetchLevel1FightingPokemon()
+fetchLevel2FightingPokemon()
